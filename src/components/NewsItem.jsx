2044bde1acd9aacd2048df30998223f4d8900166
Anuraj Singh <112105681+anurajsingh416@users.noreply.github.com>
2024-06-18 15:46:44 +0530

    Update NewsItem.jsx

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -6,7 +6,7 @@ export default function NewsItem({name,author,title,description,url,urlToImage,c
             
             <div className="flex justify-around items-center bg-gray-200  p-y-4">
                 <div className="p-6 my-2 m-4 bg-white flex flex-row md:flex-row lg:flex-row w-full max-w-xl md:max-w-3xl lg:max-w-3xl overflow-hidden items-center  shadow-2xl rounded-lg">
-                    <div className="flex flex-col space-y-1 mr-2">
+                    <div className="flex flex-col space-y-1 mr-2 flex-grow">
                         <Link to={url} target="_blank" rel="" className="block">
                             <h2 className="font-bold text-md mg:text-xl lg:text-xl hover:underline">
                                 {title}
@@ -21,7 +21,7 @@ export default function NewsItem({name,author,title,description,url,urlToImage,c
                             By {author}
                         </p>
                     </div>
-                    <div className="max-w-32 md:max-w-64 lg:max-w-64">
+                    <div className="max-w-32 md:max-w-64 lg:max-w-64 float-right">
                         <img
                             src={urlToImage}
                             className="w-full max-h-40 rounded"
